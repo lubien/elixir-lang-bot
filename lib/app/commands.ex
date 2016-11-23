e6f08c23f6fb4825e28f5c2f147a0bde82ad86c4
lubien <lubien1996@gmail.com>
2016-11-22 21:55:33 -0300

    Add /hex inline command

--- lib/app/commands.ex
+++ lib/app/commands.ex
@@ -1,7 +1,52 @@
 defmodule App.Commands do
   use App.Commander
 
+  @bot_name Application.get_env(:app, :bot_name)
+
+  command "help" do
+    message = """
+    <b>Showing Help</b>
+
+    <b>Commands</b>
+    <i>For normal commands, just send me a message.</i>
+
+    /help
+
+    <b>Inline Commands</b>
+    <i>For inline commands, type "@#{@bot_name}" then the command.</i>
+
+    <code>/hex [package_name]</code>
+    """
+
+    {:ok, _} = send_message message, parse_mode: "html"
+  end
+
+  inline_query_command "hex" do
+    "/hex" <> query = update.inline_query.query
+                      |> String.trim
+
+    {:ok, packages} = App.Hex.packages query
+
+    results = packages
+    |> Enum.take(20)
+    |> Enum.map(&App.Hex.package_to_inline_query_result/1)
+
+    case answer_inline_query(results) do
+      :ok -> nil
+      {:error, %Model.Error{reason: reason}} ->
+        Logger.log :error, "Errored with query '#{query}'"
+        Logger.log :error, "Reason: #{reason}"
+    end
+  end
+
+  inline_query do
+    answer_inline_query []
+  end
+
   message do
-    send_message "Sorry, there are no current commands in this bot"
+    send_message """
+    Sorry, I didn't undestand.
+    Try running the /help command.
+    """
   end
 end
---
+++ lib/app/hex.ex
@@ -0,0 +1,102 @@
+defmodule App.Hex do
+  require Logger
+
+  @endpoint "https://hex.pm/api"
+
+  # Public API
+
+  def packages(search, sort \\ "downloads") do
+    url = @endpoint <> "/packages"
+
+    action = fn ->
+      HTTPoison.get url, [{"Accept", "application/json"}], params: [search: search, sort: sort]
+    end
+
+    process_request(action)
+  end
+
+  def package_to_inline_query_result(package) do
+    url = package["url"]
+    name = package["name"]
+    description = get_in(package, ["meta", "description"])
+    github = get_in(package, ["meta", "links", "github"]) ||
+             get_in(package, ["meta", "links", "GitHub"])
+    image = if not is_nil(github) do
+      owner = github
+      |> String.split("/")
+      |> Enum.at(3)
+
+      "https://github.com/#{owner}.png"
+    else
+      nil
+    end
+
+    hex_link = {"Hex Package", url}
+    links = if not is_nil(github) do
+      github_link = {"GitHub Repo", github}
+
+      [github_link, hex_link]
+    else
+      [hex_link]
+    end
+
+    # I've choosen to use HTML in this case because sometimes
+    # there is text that mess with Telegram's markdown parser.
+    message = """
+    <b>#{HtmlEntities.encode name}</b>
+    <i>#{HtmlEntities.encode description}</i>
+    #{links
+      |> Enum.map(fn {title, url} ->
+        "<a href='#{url}'>#{title}</a>"
+      end)
+      |> Enum.join(" | ")}
+    """
+
+    %Nadia.Model.InlineQueryResult.Article{
+      id: url,
+      title: name,
+      description: description,
+      thumb_url: image,
+      input_message_content: %{
+        message_text: message,
+        parse_mode: "html"
+      }
+    }
+  end
+
+  # Private API
+
+  defp process_request(action) do
+    action.()
+    case action.() do
+      {:ok, %{status_code: 429, headers: headers}} ->
+        get_rate_limit(headers)
+        |> wait_rate_time_limit
+
+        process_request(action)
+      {:ok, %{status_code: 200, body: body}} ->
+        {:ok, Poison.decode! body}
+      {:error, err} ->
+        Logger.log :error, "Hex request error"
+        Logger.log :error, err
+        {:error, err}
+    end
+  end
+
+  defp get_rate_limit(headers) do
+    headers
+    |> Enum.find(fn
+      {"X-Ratelimit-Reset", value} -> value
+      _ -> false
+    end)
+    |> elem(1)
+  end
+
+  defp wait_rate_time_limit(reset_time) do
+    reset_time = String.to_integer reset_time
+    now = Calendar.DateTime.now!("UTC")
+          |> Calendar.DateTime.Format.unix
+
+    :timer.sleep (reset_time - now) * 1000
+  end
+end
