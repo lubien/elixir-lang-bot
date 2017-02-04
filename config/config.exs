b6ada78ddca4261173fed544e937fb29a72ee616
lubien <lubien1996@gmail.com>
2017-02-04 19:14:53 -0300

    Move StatsD config to global

--- config/config.exs
+++ config/config.exs
@@ -8,4 +8,9 @@ config :app,
 config :nadia,
   token: System.get_env("BOT_TOKEN")
 
+config :ex_statsd,
+       host: "localhost",
+       port: 8125,
+       namespace: "elixirstatus"
+
 import_config "#{Mix.env}.exs"
--- config/dev.exs
+++ config/dev.exs
@@ -1,6 +1,1 @@
 use Mix.Config
-
-config :ex_statsd,
-       host: "localhost",
-       port: 8125,
-       namespace: "elixirstatus"
