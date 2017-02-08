29ba4fefabc6aa24bb0ede130004c15f75eee32a
lubien <lubien1996@gmail.com>
2017-02-08 00:02:18 -0300

    Use ID for dev channels

--- config/config.exs
+++ config/config.exs
@@ -3,7 +3,8 @@ use Mix.Config
 config :app,
   storage_dir: "./storage",
   bot_name: "elixir_lang_test_bot",
-  elixirstatus_channel: "@elixirstatus_dev",
+  elixirstatus_channel: "-1001088152256",
+  pctguama_channel: "-1001110185325",
   pctguama_channel: "@pctguama_dev"
 
 config :nadia,
