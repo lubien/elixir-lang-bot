5255110d88fbb33cce3deaf715b0c3e81cc58c36
lubien <lubien1996@gmail.com>
2017-02-08 00:26:39 -0300

    Use /r/Elixir rss for new entries

--- lib/app/feed/r_elixir.ex
+++ lib/app/feed/r_elixir.ex
@@ -2,7 +2,7 @@ defmodule App.Feed.RElixir do
   @behaviour App.Feed
   @feed_id "rElixir"
   @channel Application.get_env(:app, :relixir_channel)
-  @url "https://www.reddit.com/r/elixir/.rss"
+  @url "https://www.reddit.com/r/elixir/new.rss"
   @date_format "rfc3339"
   @render_mode "html"
   use App.Feed.Rss
